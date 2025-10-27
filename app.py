2fff28628cf3a20aa29435fc190012490756a59b
mia <nasiri@devops.local>
2025-10-27 17:59:32 +0100

    ajout de fichier yml

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
- print("Hello Devops!")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello DevOps students!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
